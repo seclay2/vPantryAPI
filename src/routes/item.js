0f63bc1431d985e1dc9fcbe8ea2eb367122fd52b
seclay2 <scotteclay2@gmail.com>
2020-04-29 15:12:49 -0600

    Update item.js
    get: send full array of items instead of only first item

--- src/routes/item.js
+++ src/routes/item.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
 	var items = await ItemService.getGroupItems(req.query.group_id)
 	var itemsRet = []
-	if(items.length)
+	if(items.length) {
 		for(var i = 0; i<items.length; i++){
 			var user = await UserService.getUser(items[i].userId)
 			var item = {}
@@ -57,8 +57,9 @@ router.get('/', async (req, res) => {
 			item['createdAt'] = items[i].createdAt
 			item['updatedAt'] = items[i].updatedAt
 			itemsRet.push(item)
-			res.send({success:true, items:itemsRet})
 		}
+		res.send({success:true, items:itemsRet})
+	}
 	else
 		res.send({success:false, message : messages.no_items})
 })
